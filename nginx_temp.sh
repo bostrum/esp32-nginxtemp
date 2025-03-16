326344ecdf409e83d265ea2ef8744583983186fc
Felix Boström <46707048+bostrum@users.noreply.github.com>
2025-03-16 15:33:12 +0100

    updated var

--- nginx_temp.sh
+++ nginx_temp.sh
@@ -3,9 +3,9 @@ LOG_FILE="/srv/nginx/logs/access.log"
 OUTPUT_FILE="/srv/nginx/html/temp.txt"
 
 # get time and temp from nginx access log
-latest_entry=$(grep 'badtemp' "$LOG_FILE" | tail -n 1)
+latest_entry=$(grep 'temperature' "$LOG_FILE" | tail -n 1)
 timestamp=$(echo "$latest_entry" | grep -oP '\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}')
-temperature=$(echo "$latest_entry" | grep -oP 'badtemp=\K[\d.]+')
+temperature=$(echo "$latest_entry" | grep -oP 'temperature=\K[\d.]+')
 
 # get date from nginx format
 month_map=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec")
